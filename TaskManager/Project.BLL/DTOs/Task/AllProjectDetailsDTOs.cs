9c5d054ff984a924e21bfe0c0d2c18d6c45a54ae
Nitesh K <niteshkafle23@gmail.com>
2026-01-05 14:38:52 +0545

    Feature: Added GetAllprojectDetails Successfully

---
+++ TaskManager/Project.BLL/DTOs/Task/AllProjectDetailsDTOs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.BLL.DTOs.Task
+{
+    public record AllProjectDetailsDTOs
+    (
+        string Id,
+            string name,
+            string description,
+            DateTime createdAt
+        );
+}
